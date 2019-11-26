81611de57630cfafbe827fe32d4cc1fa1b47fb26
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-11-26 11:15:24 -0500

    change localhost to heroku

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://cyg-beta.herokuapp.com/api"
-  baseURL: "http://localhost:8000/api"
+  baseURL: "https://cyg-beta.herokuapp.com/api"
+  // baseURL: "http://localhost:8000/api"
 });
 
 // export const getAllComments = () => api.get("/comments");
